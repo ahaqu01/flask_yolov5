60007365462db440a301cad00f8757f14ee1d120
zhouyuhua <zhouyuhua@ictidei.cn>
2022-03-08 17:28:02 +0800

    commit flask yolov5

---
+++ app.py
@@ -0,0 +1,31 @@
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+
+
+
+@app.route('/video_start')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True, port=5001)
